a045cf4648545718627c9d3e5f8118b76046b4af
Vivek Kaushal <124867828+Vivekkaushal00@users.noreply.github.com>
2025-01-13 13:30:49 +0530

    add page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:api_integration/call.dart';
+import 'package:api_integration/posts.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Flutter Demo',
-      home: Call(),
+      home: Posts(),
     );
   }
 }
---
+++ lib/posts.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+
+class Posts extends StatefulWidget {
+  const Posts({super.key});
+
+  @override
+  State<Posts> createState() => _PostsState();
+}
+
+class _PostsState extends State<Posts> {
+  final Dio dio = Dio();
+
+  List posts = [];
+
+  void fetchPosts() async {
+    try {
+      final Response response =
+          await dio.get('https://jsonplaceholder.typicode.com/posts');
+
+      if (response.statusCode == 200) {
+        setState(() {
+          posts = response.data;
+        });
+      } else {
+        print("Error");
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchPosts();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Posts'),
+      ),
+      body: ListView.builder(
+        itemCount: posts.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.all(10),
+            child: ListTile(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)),
+              tileColor: Colors.grey.shade300,
+              title: Text(posts[index]['title']),
+              subtitle: Text(posts[index]['body']),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
